b7a5495aff60cf3bc3900357fe808c311cdd78af
Cams134 <48691308+Cams134@users.noreply.github.com>
2020-06-29 13:46:28 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils.timezone import utc
+from multiselectfield import MultiSelectField
+
+Sintomases = (
+            ('Fie', 'Fiebre'), ('ToS', 'Tos seca'), ('Can', 'Cansancio'), ('MoD','Molestias y Dolores'), ('Gar', 'Dolor de Garganta'), ('Diar', 'Diarrea'),  ('Conj','Conjuntivitis'), ('DC','Dolores de cabeza'), ('Per','Pérdida del olfato o del gusto'), ('PerC','Erupciones cutáneas o pérdida del color en extremidades'), ('Resp', 'Insuficiencia Respiratorias'),('Dif','Dificultad para respirar o sensación de falta de aire'), ('DolPe', 'Dolor o presión en el pecho'), ('Inc','Incapacidad para hablar o moverse')
+            )
+      
+
+class registros(models.Model):
+    dia = models.DateTimeField()
+    covid = models.CharField(max_length=20)
+    sintomas = MultiSelectField(choices=Sintomases)
+    def publish(self):
+        self.save()
