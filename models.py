54245f169992d3d75e94bd1f80eda20251073f81
Cams134 <48691308+Cams134@users.noreply.github.com>
2020-07-09 13:55:40 -0400

    Update models.py

--- models.py
+++ models.py
@@ -8,7 +8,6 @@ Sintomases = (
             ('Fie', 'Fiebre'), ('ToS', 'Tos seca'), ('Can', 'Cansancio'), ('MoD','Molestias y Dolores'), ('Gar', 'Dolor de Garganta'), ('Diar', 'Diarrea'),  ('Conj','Conjuntivitis'), ('DC','Dolores de cabeza'), ('Per','Pérdida del olfato o del gusto'), ('PerC','Erupciones cutáneas o pérdida del color en extremidades'), ('Resp', 'Insuficiencia Respiratorias'),('Dif','Dificultad para respirar o sensación de falta de aire'), ('DolPe', 'Dolor o presión en el pecho'), ('Inc','Incapacidad para hablar o moverse')
             )
       
-
 class registros(models.Model):
     dia = models.DateTimeField()
     covid = models.CharField(max_length=20)
@@ -16,5 +15,4 @@ class registros(models.Model):
 
     def publish(self):
         self.save()
-        return (self.dia, self.covid, self.sintomas)
 
